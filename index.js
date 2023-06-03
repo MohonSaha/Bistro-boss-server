f6326183aed907b1c1dabc0a5f11328f46dccfc9
MohonSaha <practicewithmohon@gmail.com>
2023-06-03 17:07:38 +0600

    create a api to update user role

--- index.js
+++ index.js
@@ -60,6 +60,21 @@ async function run() {
     })
 
 
+    // API to update user role like ADMIN:
+    app.patch('/users/admin/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
+
     // Menu related apis
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray()
