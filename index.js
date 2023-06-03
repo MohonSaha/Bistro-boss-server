84ef9574d566689567d7268c6c26d753cee83d18
MohonSaha <practicewithmohon@gmail.com>
2023-06-03 16:33:00 +0600

    create api for load all user

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ async function run() {
 
 
     // user Related apis:-
+
+
+    app.get('/users', async(req, res) => {
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+    })
+
+
     app.post('/users', async(req, res)=>{
       const user = req.body;
 
