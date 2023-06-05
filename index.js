81b6f5c72bb2d0ca194507cff3e5869ad98c704a
MohonSaha <practicewithmohon@gmail.com>
2023-06-05 10:01:36 +0600

    create admin secure delete api to delete item for admin

--- index.js
+++ index.js
@@ -165,6 +165,14 @@ async function run() {
     })
 
 
+    app.delete('/menu/:id',verifyJWT, verifyAdmin, async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
 
 
     // reviews related apis
